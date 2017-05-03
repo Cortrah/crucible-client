79725c0bd7d8d6d29ca7db45190ddc121752205b
Cortlandt Winters <cort@cortwinters.net>
2017-05-03 07:50:19 -0400

    add a basic prop

--- src/components/Client.vue
+++ src/components/Client.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="client">
-    <h2>{{ msg }}</h2>
-  </div>
+    <div class="client">
+        <h2>{{ msg + st.thing1 }}</h2>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'client',
-    data () {
-      return {
-        msg: 'client'
-      }
+    export default {
+        name: 'client',
+        props: ['st'],
+        data () {
+            return {
+                msg: 'client'
+            }
+        }
     }
-  }
 </script>
 
 <style>
--- src/components/Monitor.vue
+++ src/components/Monitor.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="monitor">
-    <h1>{{ msg }}</h1>
-    <client id="client1"></client>
-    <client id="client2"></client>
-  </div>
+    <div class="monitor">
+        <h1>{{ msg }}</h1>
+        <client id="client1" :st="st" ></client>
+        <client id="client2" :st="st" ></client>
+    </div>
 </template>
 
 <script>
-  import Client from './Client'
+    import Client from './Client'
 
-  export default {
-    name: 'monitor',
-    components: {Client},
-    data () {
-      return {
-        msg: 'monitor'
-      }
+    export default {
+        name: 'monitor',
+        components: {Client},
+        data () {
+            return {
+                st: {
+                    thing1: 1,
+                    thing2: 2,
+                },
+                msg: 'monitor'
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
