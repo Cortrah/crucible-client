2aff4723a088f4609a3521480701f89c906ef5d2
Cortlandt Winters <cort@cortwinters.net>
2017-05-04 06:59:17 -0400

    add rules and cards to crucible class

--- src/components/Crucible.vue
+++ src/components/Crucible.vue
@@ -15,8 +15,72 @@
         data () {
             return {
                 state: {
-                    thing1: 1,
-                    thing2: 2,
+                    rules:{
+                        maxMana: 10,
+                        maxHealth: 30,
+                        startingDeckSize: 20,
+                        startingHandSize: 0,
+                        maxCards:5,
+                        manaGrowthRate:1/3,
+                        manaReplentishRate:1/1,
+                        drawRate:1/1,
+                        fireRate:2/1,
+                        bleedoutRate:1/2,
+                        sheildDecayRate: 1/1
+                    },
+                    waypoint:{
+                    },
+                    players:[
+                    ],
+                    avatars:[
+                    ],
+                    cards:{
+                        "0": {
+                            cost: 0,
+                            mistleName: "nerfball",
+                            shieldName: "paper bag"
+                        },
+                        "1": {
+                            cost: 1,
+                            mistleName: "fastball",
+                            shieldName: "catchers mitt"
+                        },
+                        "2": {
+                            cost: 2,
+                            mistleName: "old rounds",
+                            shieldName: "golf umbrella"
+                        },
+                        "3": {
+                            cost: 3,
+                            mistleName: "bb bolt",
+                            shieldName: "elephant hide"
+                        },
+                        "4": {
+                            cost: 4,
+                            mistleName: "magnum round",
+                            shieldName: "flack jacket"
+                        },
+                        "5": {
+                            cost: 5,
+                            mistleName: "angry man",
+                            shieldName: "rage deflector"
+                        },
+                        "6": {
+                            cost: 6,
+                            mistleName: "mega bolt",
+                            shieldName: "mega absorber"
+                        },
+                        "7": {
+                            cost: 7,
+                            mistleName: "neutrino driver",
+                            shieldName: "neutrino barrier"
+                        },
+                        "8": {
+                            cost: 8,
+                            mistleName: "phase torpedo",
+                            shieldName: "phase shield"
+                        }
+                    }
                 },
                 msg: 'crucible'
             }
