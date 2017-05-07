b15e267a39381e534c5449b7e0da423c1fe6065e
Cortlandt Winters <cort@cortwinters.net>
2017-05-07 19:26:38 -0400

    add a few fleshed out cards

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="Card">
+        <span>{{ label }}</span>
+        <span>{{ cost }}</span>
+        <span>{{ type }}</span>
+        <img :src="face">
+        <span>{{ effect }}</span>
+    </div>
+</template>
+
+<script type="text/babel">
+
+    export default {
+        name: "Card",
+        props: ["label", "cost", "type", "face", "effect"],
+        methods: {
+            select(e){
+                return this;
+            },
+            apply: function(target){
+                return target.apply(this);
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    .Card {
+        width: 130px;
+        height: 210px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    img {
+        width: 100px;
+        height: 100px;
+    }
+</style>
--- src/components/Crucible.vue
+++ src/components/Crucible.vue
@@ -5,16 +5,21 @@
             <helm id="0" :model="game.waypoint.players[0]" :game="game" ></helm>
             <helm id="1" :model="game.waypoint.players[1]" :game="game" ></helm>
         </div>
+        <div class="cards-container">
+            <card label="Nerfball" cost="mana-0" type="mistle" face="./static/dog1.png" effect="damage+0"></card>
+            <card label="Paper Bag" cost="mana-0" type="shield" face="./static/dog1.png" effect="shield+0"></card>
+        </div>
     </div>
 </template>
 
 <script>
     import Player from './Player'
+    import Card from './Card'
     import Helm from './Helm'
 
     export default {
         name: 'Waypoint Crucible',
-        components: {Player, Helm},
+        components: {Player, Helm, Card},
         data () {
             return {
                 game: {
@@ -137,51 +142,98 @@
                         { id: '8', name: 'General Scum', img: '../static/general_scum.png' },
                         { id: '9', name: 'Admiral Hope', img: '../static/admiral_hope.png' }
                     ],
-                    cards:{
+                    mistleCards:{
                         "0": {
                             cost: 0,
-                            mistleName: "nerfball",
-                            shieldName: "paper bag"
+                            name: "nerfball",
+                            type: 'mistle'
                         },
                         "1": {
                             cost: 1,
-                            mistleName: "fastball",
-                            shieldName: "catchers mitt"
+                            name: "fastball",
+                            type: 'mistle'
                         },
                         "2": {
                             cost: 2,
-                            mistleName: "old rounds",
-                            shieldName: "golf umbrella"
+                            name: "old rounds",
+                            type: 'mistle'
                         },
                         "3": {
                             cost: 3,
-                            mistleName: "bb pellet",
-                            shieldName: "elephant hide"
+                            name: "bb pellet",
+                            type: 'mistle'
                         },
                         "4": {
                             cost: 4,
-                            mistleName: "magnum round",
-                            shieldName: "flack jacket"
+                            name: "magnum round",
+                            type: 'mistle'
                         },
                         "5": {
                             cost: 5,
-                            mistleName: "angry man",
-                            shieldName: "rage deflector"
+                            name: "angry man",
+                            type: 'mistle'
                         },
                         "6": {
                             cost: 6,
-                            mistleName: "mega bolt",
-                            shieldName: "mega absorber"
+                            name: "mega bolt",
+                            type: 'mistle'
                         },
                         "7": {
                             cost: 7,
-                            mistleName: "neutrino driver",
-                            shieldName: "neutrino barrier"
+                            name: "neutrino driver",
+                            type: 'mistle'
                         },
                         "8": {
                             cost: 8,
-                            mistleName: "phase torpedo",
-                            shieldName: "phase shield"
+                            name: "phase torpedo",
+                            type: 'mistle'
+                        }
+                    },
+                    shieldCards:{
+                        "0": {
+                            cost: 0,
+                            name: "paper bag",
+                            type: 'shield'
+                        },
+                        "1": {
+                            cost: 1,
+                            name: "catchers mitt",
+                            type: 'shield'
+                        },
+                        "2": {
+                            cost: 2,
+                            name: "golf umbrella",
+                            type: 'shield'
+                        },
+                        "3": {
+                            cost: 3,
+                            name: "elephant hide",
+                            type: 'shield'
+                        },
+                        "4": {
+                            cost: 4,
+                            name: "flack jacket",
+                            type: 'shield'
+                        },
+                        "5": {
+                            cost: 5,
+                            name: "rage deflector",
+                            type: 'shield'
+                        },
+                        "6": {
+                            cost: 6,
+                            name: "mega absorber",
+                            type: 'shield'
+                        },
+                        "7": {
+                            cost: 7,
+                            name: "neutrino barrier",
+                            type: 'shield'
+                        },
+                        "8": {
+                            cost: 8,
+                            name: "phase shield",
+                            type: 'shield'
                         }
                     }
                 },
@@ -193,6 +245,11 @@
 
 <style scoped>
     .players-container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+    .cards-container {
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
