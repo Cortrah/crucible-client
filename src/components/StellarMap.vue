e1b84288e2fed327b4b4a065136d3c14d3a1a858
Cortlandt Winters <cort@cortwinters.net>
2017-05-13 10:02:11 -0400

    improve the appearance of the stellar map

--- src/components/StellarMap.vue
+++ src/components/StellarMap.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="stellar-map">
-        <svg>
-            <circle  v-for='star in stars' :cx='star.x' :cy='star.y' r='2px' fill='#fff'></circle>
+        <svg height="80" width="650">
+            <circle  v-for='star in stars' :cx='star.x' :cy='star.y' :r='star.r' fill='#fff'></circle>
         </svg>
     </div>
 </template>
@@ -16,27 +16,29 @@
             return {
                 'name': "Solar",
                 'code': "Sol",
-                'height': 100,
-                'width': 800,
+                'height': 80,
+                'width': 650,
                 'stars': []
             }
         },
 
         created: function () {
-            for (var i = 0; i < 1000; ++i) {
+            for (var i = 0; i < 100; ++i) {
                 this.stars.push({
-                    x: Math.random() * 800,
-                    y: Math.random() * 100,
+                    x: Math.random() * 650,
+                    y: Math.random() * 80,
+                    r: Math.random() * 2,
                 })
             }
+            console.log(this.stars.toString());
         }
     }
 </script>
 
 <style>
     .stellar-map {
-        height: 100px;
-        width: 800px;
+        height: 80px;
+        width: 650px;
         padding: 0;
         margin: 0;
         background-color: black;
