ee6f4ec3e7b5bd6937510c5965d81550bd0a484a
Cortlandt Winters <cort@cortwinters.net>
2017-06-11 23:05:21 -0400

    add host splash and tabletop

---
+++ src/components/Host.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="stage">
+        <h1>{{ title }}</h1>
+
+        <label for="tableName">Name</label>
+        <input id="tableName" v-model="tableName" placeholder="Name">
+
+        <p>
+            choose some settings
+        </p>
+        <p>
+           invite some players
+        </p>
+        <button @click='hostGame()'
+                class="pure-button pure-button-primary">
+            Create Table
+        </button>
+    </div>
+</template>
+
+<script type="text/babel">
+
+    export default {
+        name: 'Host',
+        props: ['store'],
+        data () {
+            return {
+                title: 'Host a Table',
+                tableName: "X's Table"
+            }
+        },
+        methods: {
+            hostGame: function () {
+                // this.$children;
+                this.store.createTable(this.tableName);
+                let elem = document.getElementById('stage');
+                window.TweenMax.to(elem, 0.5,
+                    {height: 400, onComplete: this.go('tabletop')});
+            },
+            go: function (route) {
+                this.$route.router.go('/' + route);
+            }
+        }
+    }
+</script>
---
+++ src/components/Splash.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="stage">
+        <img class="logo" src="../assets/cav.png">
+        <div class="splash">
+            <h1>{{ title }}</h1>
+        </div>
+    </div>
+</template>
+
+<script type="text/babel">
+
+    export default {
+        name: 'Splash',
+        data () {
+            return {
+                title: 'Casual Card Table',
+            }
+        }
+    }
+</script>
+
+<style>
+    .logo {
+        width: 151px;
+        height: 185px
+    }
+</style>
