564b618715ceb65e62ce4502b031c016597880b5
Cortlandt Winters <cort@cortwinters.net>
2017-09-07 07:50:41 -0400

    dont use vue-router-sync yet after all

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex'
 import Router from 'vue-router';
-import { sync } from 'vuex-router-sync';
+//import { sync } from 'vuex-router-sync';
 
 import Home from './Home.vue';
 import router from './router.js';
@@ -9,12 +9,11 @@ import store from './store/store.js'
 
 Vue.use(Vuex);
 Vue.use(Router);
-Vue.use(VueProgress);
 
 Vue.config.productionTip = false;
 Vue.config.devTools = true;
 
-sync(store, router);
+//sync(store, router);
 
 new Vue({
     el: '#main',
